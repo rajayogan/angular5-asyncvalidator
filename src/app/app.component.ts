7b32ed8fddf993d30fde2994ea9062d64a1db182
rajayogan <rajayogan27@gmail.com>
2017-11-22 17:03:18 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  userdata = {
+    email: '',
+    displayName: ''
+  }
+  status = '';
+  constructor(private afauth: AngularFireAuth, private afs: AngularFirestore) {
+
+  }
+
+  emailcheck($event) {
+    let q = $event.target.value;
+    if (q.trim() == '') {
+      this.status = 'Email is required';
+    }
+    else {
+      this.status = 'Checking.. !!'
+      let collref = this.afs.collection('users').ref;
+      let queryref = collref.where('email', '==', q);
+      queryref.get().then((snapShot) => {
+        if (snapShot.empty) {
+          this.status = 'valid';
+        }
+        else {
+          this.status = 'Email already registered in the system, please login.. !!'
+        }
+      })
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth} from 'angularfire2/auth';
+import { AngularFirestoreModule} from 'angularfire2/firestore';
+
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +14,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence()
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
